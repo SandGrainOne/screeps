f1e5b3566eb6d78171fdf128ddfd5b0eafe8fee1
Terje H <terje.holene@gmail.com>
2018-08-08 21:45:50 +0200

    Initial project setup
    Created a project folder with grunt, grunt for screeps and screeps API typings. Actual code temporarily removed.

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function (grunt) {
+    let config = require('./.secret.json');
+
+    var branch = grunt.option('branch') || config.branch;
+    var ptr = grunt.option('ptr') ? true : config.ptr;
+
+    grunt.loadNpmTasks('grunt-screeps');
+
+    grunt.initConfig({
+        screeps: {
+            options: {
+                email: config.email,
+                password: config.password,
+                branch: branch,
+                ptr: ptr
+            },
+            dist: {
+                src: ['src/*.js']
+            }
+        }
+    });
+};
